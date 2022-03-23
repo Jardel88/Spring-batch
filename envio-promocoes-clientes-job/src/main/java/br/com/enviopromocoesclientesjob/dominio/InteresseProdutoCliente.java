93a97794217262f6cdace9da16c9f00d77b5f4b8
jldv <jardel.gomes@gft.com>
2022-03-23 10:34:50 -0300

    Configuracoes iniciais, Job e Step

---
+++ envio-promocoes-clientes-job/src/main/java/br/com/enviopromocoesclientesjob/dominio/InteresseProdutoCliente.java
@@ -0,0 +1,21 @@
+package br.com.enviopromocoesclientesjob.dominio;
+
+public class InteresseProdutoCliente {
+	
+	private Cliente cliente;
+	private Produto produto;
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+}
